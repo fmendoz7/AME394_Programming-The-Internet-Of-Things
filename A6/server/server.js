47a64792bd508592ac0675e33bad74b1588f6a9e
brightsniper99 <francissamuelmendoza7@gmail.com>
2018-11-27 15:49:14 -0700

    Debugging

--- A6/server/server.js
+++ A6/server/server.js
@@ -1,9 +1,4 @@
-var nodemailer = require('nodemailer');
-let transporter = nodemailer.createTransport('smtp://ame394fall2018%40gmail.com:fmendoz7@asu.edu');
-
-//nA: mongo
 var MS = require("mongoskin");
-
 var express = require("express");
 var app = express();
 var bodyParser = require('body-parser');
@@ -11,79 +6,107 @@ var errorHandler = require('errorhandler');
 var methodOverride = require('method-override');
 var hostname = process.env.HOSTNAME || 'localhost';
 var port = 1234;
-var lastSend = 0;
+var VALUEt = 0;
+var VALUEh = 0;
+var VALUEtime = 0;
+var tempCheck = 0;
+var nodemailer = require('nodemailer');
 
-//nA: mongoskin
+var transporter = nodemailer.createTransport('smtp://ame394fall2018%40gmail.com:francissamuelmendoza7@gmail.com');
+
+//var db = MS.db("mongodb://root:46Jl57IDy3Ji@127.0.0.1:27017/sensorData")
 var db = MS.db("mongodb://root:OfaZ13Q6DERS@localhost:27017/sensorData");
-
-app.get("/setValue", function (req, res)
+app.get("/", function (req, res) {
+    res.redirect("/index.html");
+});
+function sendEmail(temp, time)
 {
+  var message =
+  {
+      // Comma separated list of recipients
+      to: 'Francis Mendoza <fmendoz7@asu.edu>',
+      subject: 'Button pressed',
+      // plaintext body
+
+      //MODIFY TO GET REAL TIME STATEMENT
+      text: 'Button pressed!',
+      // HTML body
+
+      //Can view as plain text HTML
+      html:  '<p>Your button was presssed </p>',
+      watchHtml:  '<p>Your button was presssed </p>'
+    }
+
+
+    console.log('Sending Mail');
+    transporter.sendMail(message, function(err, result){
+        console.log(err,result);
+    });
+
+
+}
+
+app.get("/getAverage", function (req, res) {
+  //res.writeHead(200, {'Content-Type': 'text/plain'});
+  var from = parseInt(req.query.from);
+  var to = parseInt(req.query.to);
+
+  db.collection("data").find({time:{$gt:from, $lt:to}}).toArray(function(err, result){
+  	console.log(err);
+  	console.log(result);
+  	var tempSum = 0;
+  	var humSum = 0;
+  	for(var i=0; i< result.length; i++){
+  		tempSum += result[i].t || 0;
+  		humSum += result[i].h || 0;
+  	}
+  	var tAvg = tempSum/result.length;
+  	var hAvg = humSum/result.length;
+    res.send(tAvg.toString() + " " + hAvg.toString() + " " + from.toString() + "\r");
+  });
+
+});
+
+app.get("/getLatest", function (req, res) {
+    db.collection("data").find({}).sort({time:-1}).limit(10).toArray(function(err, result){
+        res.send(JSON.stringify(result));
+    });
+});
+
+app.get("/getData", function (req, res) {
+    var from = parseInt(req.query.from);
+    var to = parseInt(req.query.to);
+    db.collection("data").find({time:{$gt:from, $lt:to}}).sort({time:-1}).toArray(function(err, result){
+        res.send(JSON.stringify(result));
+    });
+});
+app.get("/getValue", function (req, res) {
+  //res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.send(VALUEt.toString() + " " + VALUEh + " " + VALUEtime + "\r");
+});
+
+app.get("/setValue", function (req, res) {
   VALUEt = parseFloat(req.query.t);
   VALUEh = parseFloat(req.query.h);
   VALUEtime = new Date().getTime();
-  var id = req.query.id;
-	var dataObj =
-  {
+	var dataObj = {
 		t: VALUEt,
 		h: VALUEh,
 		time: VALUEtime
-
-	}
-
-	if (VALUEt > 100) //100 degress
-	{
-		var now = new Date().getTime();
-
-		if(now - lastSend > 5*60*1000) //5minutes
-		{
-			console.log("The temperature is" + VALUEt + ", email sent");
-			sendEmail();
-			lastSend = now; //resets it
+	};
+	if(VALUEh > 80) {
+        var date = new Date(); // get the current date.
+        if(date.getTime() >= (tempCheck + 300000)) { // get the current time from the date object we just made, compare that to the previous date object, plus 5 minutes, if it's greater, send email
+            tempCheck = date.getTime();
+            sendEmail(VALUEt, date);
 		}
-
-	res.send("2");
-}
-	else
-	{
-		console.log("The temperature is" + " " + VALUEt + ",email not sent. Temperature needs to be above 100 to be sent.");
-		//sendEmail();
-			res.send("2");
-
 	}
-});
-
-function sendEmail()
-{
-  nodemailer.createTestAccount((err, account) => {
-
-var transporter = nodemailer.createTransport({
- service: 'gmail',
- auth: {
-        user: 'fmendoz7@asu.edu',
-        pass: 'HanchiBoy77***>>'
-    }
-});
-
-    // setup email data with unicode symbols
-    let mailOptions = {
-        from: '"Test account" <fmendoz7@asu.edu>', // sender address
-        to: 'fmendoz7@asu.edu', // list of receivers
-        subject: 'Sensor Alert', // Subject line
-        text: 'The temperature is' + ' ' + VALUEt + '!!!', // plain text body
-        html: '<b>The temperature is' + ' ' +VALUEt + '!!!</b>' // html body
-    };
-
-  	  transporter.sendMail(mailOptions, (error, info) => {
-	        if (error) {
-	            return console.log(error);
-	        }
-	        console.log('Message sent: %s', info.messageId);
-	        //Preview only available when sending through an Ethereal account
-	       console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+	db.collection("data").insert(dataObj, function(err,result){
+		console.log("added data: " + JSON.stringify(dataObj));
 	});
+  res.send(VALUEtime.toString());
+});
 
-  });
-}
 
 app.use(methodOverride());
 app.use(bodyParser());
