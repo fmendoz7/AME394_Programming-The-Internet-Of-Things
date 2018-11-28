9966fae2da6c4d1ab360c583d46dcd8c0ba49ba0
brightsniper99 <francissamuelmendoza7@gmail.com>
2018-11-27 19:26:41 -0700

    First Attempt at A7

--- A7/server/server.js
+++ A7/server/server.js
@@ -1,9 +1,13 @@
+/*
+  Programmer: Francis Mendoza
+  Date: 11/27/18, 18:40 hours MST
+  Purpose: Calculate average
+*/
+
+//Nodemailer functionality
 var nodemailer = require('nodemailer');
-
-//THIS LINE ALLOWS YOU CHOOSE THE SENDER FOR THE GMAIL ACCOUNT
-let transporter = nodemailer.createTransport('smtp://ame394fall2018%40gmail.com:francissamuelmendoza7@gmail.com');
-//##############################################################################
-
+var transporter = nodemailer.createTransport('smtp://ame394fall2018%40gmail.com:francissamuelmendoza7@gmail.com');
+//-----------------------------------------------------------------------------------------------------
 var MS = require("mongoskin");
 var express = require("express");
 var app = express();
@@ -15,20 +19,23 @@ var port = 1234;
 var VALUEt = 0;
 var VALUEh = 0;
 var VALUEtime = 0;
-
-
+var tempCheck = 0;
+//-----------------------------------------------------------------------------------------------------
 //var db = MS.db("mongodb://root:46Jl57IDy3Ji@127.0.0.1:27017/sensorData")
-var db = MS.db("mongodb://user:pass@localhost:27017/sensorData")
-app.get("/", function (req, res) {
+var db = MS.db("mongodb://root:OfaZ13Q6DERS@localhost:27017/sensorData");
+app.get("/", function (req, res)
+{
     res.redirect("/index.html");
 });
-
-
-app.get("/getAverage", function (req, res) {
+//-----------------------------------------------------------------------------------------------------
+//FUNCTION: getAverage
+app.get("/getAverage", function (req, res)
+{
   //res.writeHead(200, {'Content-Type': 'text/plain'});
   var from = parseInt(req.query.from);
   var to = parseInt(req.query.to);
 
+  //MONGODB
   db.collection("data").find({time:{$gt:from, $lt:to}}).toArray(function(err, result){
   	console.log(err);
   	console.log(result);
@@ -40,18 +47,39 @@ app.get("/getAverage", function (req, res) {
   	}
   	var tAvg = tempSum/result.length;
   	var hAvg = humSum/result.length;
-    res.send(tAvg.toString() + " " + hAvg.toString() + "\r");
+    res.send(tAvg.toString() + " " + hAvg.toString() + " " + from.toString() + "\r");
   });
 
 });
-
-
-app.get("/getValue", function (req, res) {
+//-----------------------------------------------------------------------------------------------------
+//FUNCTION: getLatest
+app.get("/getLatest", function (req, res)
+{
+    db.collection("data").find({}).sort({time:-1}).limit(10).toArray(function(err, result){
+        res.send(JSON.stringify(result));
+    });
+});
+//-----------------------------------------------------------------------------------------------------
+//FUNCTION: getData
+app.get("/getData", function (req, res)
+{
+    var from = parseInt(req.query.from);
+    var to = parseInt(req.query.to);
+    db.collection("data").find({time:{$gt:from, $lt:to}}).sort({time:-1}).toArray(function(err, result){
+        res.send(JSON.stringify(result));
+    });
+});
+//-----------------------------------------------------------------------------------------------------
+//FUNCTION: getValue
+app.get("/getValue", function (req, res)
+{
   //res.writeHead(200, {'Content-Type': 'text/plain'});
   res.send(VALUEt.toString() + " " + VALUEh + " " + VALUEtime + "\r");
 });
-
-app.get("/setValue", function (req, res) {
+//-----------------------------------------------------------------------------------------------------
+//FUNCTION: setValue
+app.get("/setValue", function (req, res)
+{
   VALUEt = parseFloat(req.query.t);
   VALUEh = parseFloat(req.query.h);
   VALUEtime = new Date().getTime();
@@ -60,41 +88,52 @@ app.get("/setValue", function (req, res) {
 		t: VALUEt,
 		h: VALUEh,
 		time: VALUEtime
-	}
-//##############################################################################
-//CONDITION TO CALL SENDEMAIL
-  if(VALUEt > 60)
+	};
+
+  //Condition for sensor to trip after 100*F
+	if(VALUEt > 100)
   {
-    sendEmail();
-  }
-//##############################################################################
+        var date = new Date(); // get the current date.
+        if(date.getTime() >= (tempCheck + 300000))
+        { //Condition to send after 5 minutes
+            tempCheck = date.getTime();
+            sendEmail(VALUEt, date);
+		    }
+	}
 	db.collection("data").insert(dataObj, function(err,result){
 		console.log("added data: " + JSON.stringify(dataObj));
 	});
   res.send(VALUEtime.toString());
 });
-//##############################################################################
-//SENDMAIL FUNCTION
-function sendEmail()
+//-----------------------------------------------------------------------------------------------------
+//FUNCTION: sendEmail
+function sendEmail(temp, time)
 {
-  let message = {
-    // Comma separated list of recipients
-    to: 'Francis Mendoza <fmendoz7@asu.edu>',
-    subject: 'Button pressed',
-    // plaintext body
-    text: 'Button pressed!',
-    // HTML body
-    html:  '<p>Your button was presssed </p>',
-    watchHtml:  '<p>Your button was presssed </p>'
-  }
+  var message =
+  {
+      // Comma separated list of recipients
+      to: 'Francis Mendoza <francissamuelmendoza7@gmail.com>',
+      subject: 'IoT Sensor Alert- Warning',
+      // plaintext body
 
-  console.log('Sending Mail');
-  transporter.sendMail(message, function(err, result){
-	console.log(err,result);
-  });
+      //MODIFY TO GET REAL TIME STATEMENT
+      text: 'Sensor tripped at ' + time + '. Temperature exceeded 100*F. Current Temperature: ' + temp,
+      // HTML body
+
+      //Can view as plain text HTML
+      html:  '<p> Sensor tripped at ' + time + '. Temperature exceeded 100*F. Current Temperature: ' + temp + '</p>',
+      watchHtml:  '<p> Sensor tripped at ' + time + '. Temperature exceeded 100*F. Current Temperature: ' + temp + '</p>'
+    }
+
+
+    console.log('Sending Mail');
+    transporter.sendMail(message, function(err, result)
+    {
+        console.log(err,result);
+    });
 }
-//##############################################################################
-
+//-----------------------------------------------------------------------------------------------------
+//ASSORTED FUNCTIONS
 app.use(methodOverride());
 app.use(bodyParser());
 app.use(express.static(__dirname + '/public'));
